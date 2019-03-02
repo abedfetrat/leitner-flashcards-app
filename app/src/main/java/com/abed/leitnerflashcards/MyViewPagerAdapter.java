345da77d2a57e6cf5fe3b1dbc120fc86f02ab959
Abed <abedfetrat@outlook.com>
2019-03-02 10:37:38 +0100

    implemented view pager

---
+++ app/src/main/java/com/abed/leitnerflashcards/MyViewPagerAdapter.java
@@ -0,0 +1,49 @@
+package com.abed.leitnerflashcards;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MyViewPagerAdapter extends PagerAdapter {
+    private Context context;
+    private List<String> data;
+
+    public MyViewPagerAdapter(Context context, List<String> data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        ViewGroup layout = (ViewGroup) inflater.inflate(R.layout.pager_item, container, false);
+
+        TextView tv = layout.findViewById(R.id.textView);
+        tv.setText(data.get(position));
+
+        container.addView(layout);
+        return layout;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view == o;
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+}
---
+++ app/src/main/java/com/abed/leitnerflashcards/ViewPagerWithoutSwipe.java
@@ -0,0 +1,29 @@
+package com.abed.leitnerflashcards;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class ViewPagerWithoutSwipe extends ViewPager {
+
+    public ViewPagerWithoutSwipe(@NonNull Context context) {
+        super(context);
+    }
+
+    public ViewPagerWithoutSwipe(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return false;
+    }
+}
